80f2f4d92bc3d94d7be0e400781f5fd7c0afcdd7
Jason <351192873@qq.com>
2014-05-01 09:20:19 +0800

    Update spFunctions.php
    增加自定义过滤函数

--- SpeedPHP/spFunctions.php
+++ SpeedPHP/spFunctions.php
@@ -3,6 +3,24 @@
 // SpeedPHP中文PHP框架, Copyright (C) 2008 - 2010 SpeedPHP.com //
 /////////////////////////////////////////////////////////////////
 
+//自定义过滤html函数
+function uhtml($str)  
+{  
+    $farr = array(  
+        "/\s+/", //过滤多余空白  
+         //过滤 <script>等可能引入恶意内容或恶意改变显示布局的代码,如果不需要插入flash等,还可以加入<object>的过滤  
+        "/<(\/?)(script|i?frame|style|html|body|title|link|meta|\?|\%)([^>]*?)>/isU", 
+        "/(<[^>]*)on[a-zA-Z]+\s*=([^>]*>)/isU",//过滤javascript的on事件  
+   );  
+   $tarr = array(  
+        " ",  
+        "＜\1\2\3＞",//如果要直接清除不安全的标签，这里可以留空  
+        "\1\2",  
+   );  
+  $str = preg_replace( $farr,$tarr,$str);  
+   return $str;  
+}
+
 /**
  * spRun  执行用户代码
  */
